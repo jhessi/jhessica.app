8ff916a475707950dadd4f2205fc74f0cf16a5aa
mayumi charaña <mayumi-24@cls>
2016-03-03 20:07:35 -0500

    iniciando proyecto

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+<div class="contenedor">
+<nav>
+   <ul class="menu-lista">
+		<li class="menu-item"><a href="" class="menu-link">INICIO</a></li>
+		<li class="menu-item"><a href="" class="menu-link">GALERIA</a></li>
+		<li class="menu-item"><a href="" class="menu-link">BLOG</a></li>
+		<li class="menu-item"><a href="" class="menu-link">ANUNCIOS</a></li>
+		<li class="menu-item"><a href="" class="menu-link">CONTÁCTENOS</a></li>
+	</ul>
+</nav>	
+<div class="slider">
+<img src="nana.jpg" height="450" width="960">
+</div>
+
+<div class="slider">
+<img src="prim.jpe" height="180" width="180">
+</div>
+
+<div class="slider">
+<img src="movi.gif" height="180" width="180">
+</div>
+
+<div class="slider">
+<img src="mmmmmmmiiiii.gif" height="180" width="180">
+</div>
+
+<div class="slider">
+<img src="imagenes-de-libro-con-frases-de-amor.gif" height="180" width="180">
+</div>
+
+</div>
+</body>
+</html>
